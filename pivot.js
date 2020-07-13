247e38edf86e780b1c68f53040829df583ebf21b
VeraDidenko <verochka1998@gmail.com>
2020-07-13 22:17:10 +0300

    updated styles

--- pivot.js
+++ pivot.js
@@ -1,8 +1,9 @@
 require('flexmonster');
-require("./d3.v3.min.js");
 
 const pivot = new Flexmonster({
     container: "pivot-container",
     toolbar: true,
-    report: "https://cdn.flexmonster.com/reports/report.json"
+    report: "https://cdn.flexmonster.com/reports/report.json",
+    width: "100%",
+    height: "100%"
 });
